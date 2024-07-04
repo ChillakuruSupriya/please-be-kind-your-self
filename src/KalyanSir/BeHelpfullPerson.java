14b1fb8d8d953439629dd95a9c88f3985d27495e
chillakurusupriya <chillakurusupriya@gmail.com>
2024-07-04 20:45:12 +0530

    no one can take care of you

---
+++ src/KalyanSir/BeHelpfullPerson.java
@@ -0,0 +1,16 @@
+package KalyanSir;
+
+public class BeHelpfullPerson 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Our Main Agenda is to teach core"
+	+ " functionality to students like as primary colours ");
+		System.out.println("Arif is a Good person");
+		
+		
+
+	}
+
+}
