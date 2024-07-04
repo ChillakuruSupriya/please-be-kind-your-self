4d53f98be07d018eb0efa3c0706c8cb6cdd3a42f
ChillakuruSupriya <chillakurusupriya@gmail.com>
2024-07-04 21:00:12 +0530

    Update BeHelpfullPerson.java

--- src/KalyanSir/BeHelpfullPerson.java
+++ src/KalyanSir/BeHelpfullPerson.java
@@ -8,6 +8,8 @@ public class BeHelpfullPerson
 		System.out.println("Our Main Agenda is to teach core"
 	+ " functionality to students like as primary colours ");
 		System.out.println("Arif is a Good person");
+	        System.out.println("Arif is a wise person");
+
 		
 		
 
